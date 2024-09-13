d4065a61e966c96ebbb349b52904b62e4914fd6d
Stefan Alfbo <stefan@alfbo.se>
2024-09-13 09:17:03 +0200

    Add history memory in the LLM chat code

--- chat.py
+++ chat.py
@@ -2,8 +2,10 @@ import os
 
 from dotenv import load_dotenv
 
+from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_openai import ChatOpenAI
-from langchain_core.prompts import ChatPromptTemplate  
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain.schema import StrOutputParser
 
 load_dotenv()
@@ -11,21 +13,33 @@ load_dotenv()
 
 chat_llm = ChatOpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"))
 
+memory = ChatMessageHistory()
+
+
+def get_memory(session_id):
+    return memory
+
+
 prompt = ChatPromptTemplate.from_messages(
     [
         (
             "system",
-            "You are a surfer dude, having a conversation about the surf conditions on the beach. Respond using surfer slang."
+            "You are a surfer dude, having a conversation about the surf conditions on the beach. Respond using surfer slang.",
         ),
         ("system", "{context}"),
-        (
-            "human",
-            "{question}"
-        ),
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("human", "{question}"),
     ]
 )
 
 chat_chain = prompt | chat_llm | StrOutputParser()
+
+chat_with_message_history = RunnableWithMessageHistory(
+    chat_chain,
+    get_memory,
+    input_messages_key="question",
+    history_messages_key="chat_history",
+)
 
 current_weather = """
 {
@@ -37,11 +51,19 @@ current_weather = """
     ]
 }"""
 
-response = chat_chain.invoke(
-    {
-        "context": current_weather,
-        "question": "What is the weather like?"
-    }
-)
+while True:
+    question = input("> ")
 
-print(response)
+    response = chat_with_message_history.invoke(
+        {
+            "context": current_weather,
+            "question": question,
+            
+        }, 
+        config={
+            "configurable": {"session_id": "none"}
+        }
+    )
+    
+    print(response)
+    
